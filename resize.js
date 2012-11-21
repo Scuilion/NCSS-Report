9b9fa81a43b1c8fba42c1b6f6e100914d617464a
kevin.oneal <oneal.kevin@gmail.com>
2012-11-21 13:05:41 -0600

    adding resize func

---
+++ resize.js
@@ -0,0 +1,54 @@
+var makeResizable = function(oTable){
+   var numberOfColumns = oTable.find('TR:first TH').size();
+   var tableId = oTable.attr('id'); 
+
+   for (var i=0; i<=numberOfColumns; i++) {
+      if ( i == 0 || i == numberOfColumns) continue;
+      //enjoy this nice chain :)
+      $('<div class="draghandle" id="'+tableId+'_id'+i+'"></div>').insertBefore(oTable).data('tableid', tableId).data('myindex',i).draggable(
+         { axis: "x",
+           start: function () 
+           {
+              var tableId = ($(this).data('tableid'));
+              $(this).toggleClass( "dragged" );
+              //set the height of the draghandle to the current height of the table, to get the vertical ruler
+              $(this).css({ height: $('#'+tableId).height() + 'px'} );
+           },
+           stop: function (event, ui) 
+           {
+              var tableId = ($(this).data('tableid'));
+              $( this ).toggleClass( "dragged" ); 
+              var oldPos  = ($( this ).data("draggable").originalPosition.left);
+              var newPos = ui.position.left;
+              var index =  $(this).data("myindex");
+              resetTableSizes($('#'+tableId), newPos-oldPos, index-1);
+           }		  
+         }
+         );
+    };
+};
+function resetSliderPositions(table) {
+   var tableId = table.attr('id'); 
+   //put all sliders on the correct position
+   table.find(' TR:first TH').each(function(index)
+   { 
+         var td = $(this);
+         var newSliderPosition = td.offset().left+td.outerWidth() ;
+         $("#"+tableId+"_id"+(index+1)).css({  left:   newSliderPosition , height: table.height() + 'px'}  );
+   });
+}
+
+function resetTableSizes (table, change, columnIndex) {
+   //calculate new width;
+   var tableId = table.attr('id'); 
+   var myWidth = $('#'+tableId+' TR TH').get(columnIndex).offsetWidth;
+   var newWidth = (myWidth+change)+'px';
+
+   $('#'+tableId+' TR').each(function() 
+   {
+      $(this).find('TD').eq(columnIndex).css('width',newWidth);
+      $(this).find('TH').eq(columnIndex).css('width',newWidth);
+   });
+   resetSliderPositions(table);
+};
+
